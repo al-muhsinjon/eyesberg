3a29a98602a9d1d2f7dc05d5cd962a3b7ceaf9d8
Mullajonov Muhsinjon <122345056+al-muhsinjon@users.noreply.github.com>
2024-04-30 09:21:50 +0500

    Responsive

--- app/(site)/tozalash/page.jsx
+++ app/(site)/tozalash/page.jsx
@@ -13,7 +13,7 @@ const Tozalik = () => {
       <main className="px-[7%] h-screen bg-hero  md:bg-[url(/images/hero.png)] bg-no-repeat bg-right ">
         <div className="2xl:w-[85%] xl:w-1/2  h-[80vh] flex flex-col py-24 justify-around p-4">
           <h1 className="md:text-6xl  text-3xl font-bold">General tozalash </h1>
-          <p className="text-gray w-[50%]">
+          <p className="text-gray md:w-[50%]">
             Bemalol o&apos;zi ishlaringiz bilan shug&apos;ullanib tozalashni
             Professionalga qo&apos;yib bering va buyurtma qining
           </p>
@@ -29,7 +29,7 @@ const Tozalik = () => {
           </h2>
           <FilterButton />
         </div>
-        <div className="grid grid-cols-3 py-6  gap-6">
+        <div className="grid md:grid-cols-3 py-6  gap-6">
           {datas.map((item) => (
             <InfoCard key={item.id} data={item} />
           ))}
@@ -39,7 +39,7 @@ const Tozalik = () => {
             <h2 className="text-xl font-bold">Ko&apos;proq ko&apos;rish</h2>
           </div>
         )}
-        <div className="grid relative -z-10 grid-cols-3 my-4 gap-8">
+        <div className="grid relative -z-10 md:grid-cols-3 my-4 gap-8">
           <div className="flex justify-center p-6 items-center">
             <h2 className="font-bold text-3xl">
               Tozalash vaqtidagi fotosuratlar
